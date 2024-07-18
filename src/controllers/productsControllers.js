bb111bd6dba929705c4c83f01d09eb647886c359
Yasser4Coder <by.benseghier.yasser@gmail.com>
2024-07-18 12:44:07 +0100

    deadline

--- src/controllers/productsControllers.js
+++ src/controllers/productsControllers.js
@@ -48,3 +48,62 @@ export const addFlash = async (req, res) => {
       .json({ message: "Server error", error: err.message });
   }
 };
+
+export const setDeadline = async (req, res) => {
+  const { deadline } = req.params;
+
+  const month = deadline.slice(0, 2);
+  const day = deadline.slice(3, 5);
+  const year = deadline.slice(6, 10);
+  let mo;
+  //07-18-2024
+  if (month === "01") {
+    mo = "January";
+  }
+  if (month === "02") {
+    mo = "February";
+  }
+  if (month === "03") {
+    mo = "March";
+  }
+  if (month === "04") {
+    mo = "April";
+  }
+  if (month === "05") {
+    mo = "May";
+  }
+  if (month === "06") {
+    mo = "June";
+  }
+  if (month === "07") {
+    mo = "July";
+  }
+  if (month === "08") {
+    mo = "August";
+  }
+  if (month === "09") {
+    mo = "September";
+  }
+  if (month === "10") {
+    mo = "October";
+  }
+  if (month === "11") {
+    mo = "November";
+  }
+  if (month === "12") {
+    mo = "December";
+  }
+
+  try {
+    if (!deadline) {
+      return res.status(400).json({ message: "some information are missing" });
+    }
+
+    const realDeadline = `${mo}, ${day}, ${year}`;
+    // "July, 20, 2024" exp;
+
+    return res.status(200).json({ deadline: realDeadline });
+  } catch (err) {
+    res.status(500).json({ message: "Server error", error: err.message });
+  }
+};
--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -4,6 +4,7 @@ import {
   addproducts,
   getAllFlash,
   addFlash,
+  setDeadline,
 } from "../controllers/productsControllers.js";
 
 const router = express.Router();
@@ -12,5 +13,6 @@ router.get("/products", getAllProducts);
 router.post("/products", addproducts);
 router.get("/flash", getAllFlash);
 router.post("/flash", addFlash);
+router.get("/deadline/:deadline", setDeadline);
 
 export default router;
